7ca0e216221f15abdeee71cf715ea4a64937280e
daniel-ldg <daniel.ldg@icloud.com>
2025-11-11 23:54:04 -0600

    fix: update token

--- src/components/header/VersionControlModal.js
+++ src/components/header/VersionControlModal.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import Timeline from "../ui/timeline/Timeline";
 
 const token = atob(
-	"Z2l0aHViX3BhdF8xMUFFQU1TMlEwTkU5a1BhOFpKVGRJX0Z3Ymh4enNFdndaUVVaaDYxWDdveWE0UjdZemtZNkxiN2RmV2JJdjc5SzZJR1pUNDIzTFpaWU5wZVVl"
+	"Z2l0aHViX3BhdF8xMUFFQU1TMlEwVmpuQzl5RlViM2R5X3B5RTFrYWNPNWdhOE5PNDhrNjFvcTZ2ZFJEbWQxaEF6ODQ1UUdTTzFUcVI2T1lVQ0ZXU0lIeWdGQXJo"
 );
 const url = "https://api.github.com/repos/daniel-ldg/nominas/commits";
 
@@ -37,18 +37,18 @@ const VersionControlModal = ({ isOpen, onClose }) => {
 		<Dialog
 			isOpen={isOpen}
 			onClose={onClose}
-			icon='git-branch'
-			title='Historial de actualizaciones'
+			icon="git-branch"
+			title="Historial de actualizaciones"
 			style={{ height: "300px", padding: "0" }}>
 			<div style={{ overflow: "auto" }}>
 				<DialogBody>
 					{!response.data && !response.error && <Spinner />}
 					{response.error && (
 						<NonIdealState
-							icon='error'
-							title='Error'
-							description='Ocurrió un error al intentar cargar los datos'
-							action={<Button text='Reintentar' onClick={doRequest} />}
+							icon="error"
+							title="Error"
+							description="Ocurrió un error al intentar cargar los datos"
+							action={<Button text="Reintentar" onClick={doRequest} />}
 						/>
 					)}
 					{response.data && (
